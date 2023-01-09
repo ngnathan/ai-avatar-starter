674cd8dac4826a706bcddee217ea521a2eae4f60
ngnathan <ng.nathan@gmail.com>
2023-01-09 16:14:17 -0500

    Updates to document

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="AI Avatar Generator" key="title" />
+        <meta property='og:title' content='SuperNate Generator' key='title' />
         <meta
-          property="og:description"
-          content="build with buildspace"
-          key="description"
+          property='og:description'
+          content='What would Nate look like if he was a superhero depicted in various artistic styles?'
+          key='description'
         />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name='twitter:card' content='summary_large_image'></meta>
       </Head>
       <body>
         <Main />
